01121b7129c92ec59dd6a58de8e44dd390c50e84
tbence94 <tihanyibence@gmail.com>
2022-06-21 15:50:48 +0200

    remove build file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sh autogen.sh
-./configure --enable-post-processing --disable-data-channels \
-    --disable-all-plugins --enable-plugin-echotest --enable-plugin-videoroom \
-    --disable-all-transports --enable-websockets
-make
-make install
